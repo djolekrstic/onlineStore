647f53b5002e87782170cb252bd0a0fe9a8a7b12
djole_krsticc <djkrstic9@gmail.com>
2023-11-26 08:20:48 +0100

    Update

--- src/components/NavSearch.jsx
+++ src/components/NavSearch.jsx
@@ -6,7 +6,7 @@ import { nanoid } from "nanoid";
 const genresQuery = () => {
   return {
     queryKey: ["genres"],
-    queryFn: () => customFetch(`/genres?key=${process.env.RAWG_KEY}`),
+    queryFn: () => customFetch(`/genres?key=${process.env.REACT_APP_RAWG_KEY}`),
   };
 };
 
