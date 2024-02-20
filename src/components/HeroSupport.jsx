4522123a677be7bcc46ff32b3f6e1bcf5157adf0
djolekrstic <djkrstic9@gmail.com>
2024-02-20 02:01:13 +0100

    Changed h5 to h1 in HeroSupport component for improved sequentially-descending order

--- src/components/HeroSupport.jsx
+++ src/components/HeroSupport.jsx
@@ -8,28 +8,28 @@ const HeroSupport = () => {
       <div className="line-item">
         <Delivery width={widthValue} />
         <div className="line-item-info">
-          <h5>Free Shipping</h5>
+          <h1>Free Shipping</h1>
           <p>On all orders over $75.00</p>
         </div>
       </div>
       <div className="line-item">
         <Box width={widthValue} />
         <div className="line-item-info">
-          <h5>Free Returns</h5>
+          <h1>Free Returns</h1>
           <p>Returns are free within 9 days</p>
         </div>
       </div>
       <div className="line-item">
         <Support width={widthValue} />
         <div className="line-item-info">
-          <h5>Support 24/7</h5>
+          <h1>Support 24/7</h1>
           <p>Contact us 24 hours a day</p>
         </div>
       </div>
       <div className="line-item">
         <Payment width={widthValue} />
         <div className="line-item-info">
-          <h5>100% Payment Secure</h5>
+          <h1>100% Payment Secure</h1>
           <p>Your payment is safe with us.</p>
         </div>
       </div>
